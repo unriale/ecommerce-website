8fd67ec5d0490c69e483fc846fd7f1e886beeb39
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2022-06-26 18:58:42 +0200

    added product serializer

---
+++ backend/base/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Product
+from django.contrib.auth.models import User
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,9 @@ from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
+from .serializers import ProductSerializer
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
@@ -21,13 +24,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
